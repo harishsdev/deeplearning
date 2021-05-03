16e1262638bae3e8edc357700bc512e9850907ea
Harish Shankam <harishsdev@gmail.com>
2021-05-03 20:29:11 +0530

    first commit for setup.py and basic requirements

---
+++ pytorch/pytorch_imple/v1/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import os
+import shutil
+import sys
+from setuptools import setup, find_packages
+
+VERSION = '0.1'
+
+long_description = '''torch-vision provides DataLoaders, Pre-trained models
+and common transforms for torch for images and videos'''
+
+excluded = ['test']
+def exclude_package(pkg):
+    for exclude in excluded:
+        if pkg.startswith(exclude):
+            return True
+    return False
+
+def create_package_list(base_package):
+    return ([base_package] +
+            [base_package + '.' + pkg
+             for pkg
+             in find_packages(base_package)
+             if not exclude_package(pkg)])
+
+
+setup_info = dict(
+    # Metadata
+    name='torchvision',
+    version=VERSION,
+    author='harishsdev',
+    author_email='harishsdev@gmail.com',
+    url='https://github.com/pytorch/vision',
+    description='image and video datasets and models for torch deep learning',
+    long_description=long_description,
+    license='BSD',
+
+    # Package info
+    packages=find_packages(exclude=('test',)), #create_package_list('torchvision'),
+
+    zip_safe=True,
+)
+
+setup(**setup_info)
+
